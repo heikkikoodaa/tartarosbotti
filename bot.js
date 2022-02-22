d9d0a0e8fff8c843b88704dfed290bb1414e897c
Hexutin <32955793+torastrengel@users.noreply.github.com>
2022-02-22 08:07:29 +0200

    Add handling Redis Client errors

--- bot.js
+++ bot.js
@@ -9,7 +9,11 @@ const Redis = require('redis');
 const redisClient = Redis.createClient({
   url: process.env.REDIS_URL
 });
-redisClient.connect();
+(async () => {
+  await redisClient.connect();
+
+  redisClient.on('error', (err) => console.error(`Redis Client Error: ${err}`));
+})();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
