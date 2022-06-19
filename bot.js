2f507bf4925263773f1b3299394be0858005607b
torastrengel <hekuttaja@gmail.com>
2022-06-20 00:01:58 +0300

    Prettify and change access token

--- bot.js
+++ bot.js
@@ -1,13 +1,24 @@
 require('dotenv').config();
 
 const { Client, Intents, MessageEmbed } = require('discord.js');
-const { containsTwitchUrl, containsUrl, getGameArt, getGameId } = require('./helpers');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_PRESENCES] });
+const {
+  containsTwitchUrl,
+  containsUrl,
+  getGameArt,
+  getGameId,
+} = require('./helpers');
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_PRESENCES,
+  ],
+});
 const PREFIX = '!';
 const DEFAULT_EXPIRATION = 28800;
 const Redis = require('redis');
 const redisClient = Redis.createClient({
-  url: process.env.REDIS_URL
+  url: process.env.REDIS_URL,
 });
 (async () => {
   await redisClient.connect();
@@ -21,10 +32,14 @@ client.on('ready', () => {
 });
 
 client.on('presenceUpdate', async (oldPresence, newPresence) => {
-  const {displayName: username} = newPresence.member;
+  const { displayName: username } = newPresence.member;
 
   //Tarkista striimin loputtua löytyykö käyttäjä Redisin muistista
-  if (newPresence.member.presence.activities.length == 0 || newPresence.member.presence.activities.length == 1 && newPresence.member.presence.activities[0].type == 'CUSTOM') {
+  if (
+    newPresence.member.presence.activities.length == 0 ||
+    (newPresence.member.presence.activities.length == 1 &&
+      newPresence.member.presence.activities[0].type == 'CUSTOM')
+  ) {
     const userStreaming = await redisClient.get(username);
     //Jos käyttäjä löytyy muistista, striimi on ollut meneillään ja käyttäjä lopettaa striimin
     if (userStreaming != null) {
@@ -36,42 +51,55 @@ client.on('presenceUpdate', async (oldPresence, newPresence) => {
   }
 
   //Tarkista onko käyttäjän uusi tila 'STREAMING' ja että uusi aktiviteettilista ei ole tyhjä
-  if (newPresence.member.presence.activities.length > 0 && newPresence.member.presence.activities[0].type == 'STREAMING') {
+  if (
+    newPresence.member.presence.activities.length > 0 &&
+    newPresence.member.presence.activities[0].type == 'STREAMING'
+  ) {
     //Tarkista löytyykö käyttäjä jo muistista - jos ei, käyttäjä aloittaa striiminsä ja kanavalle lähetetään ilmoitus
     const userStreaming = await redisClient.get(username);
     if (userStreaming == null) {
-      await redisClient.setEx(username, DEFAULT_EXPIRATION, newPresence.member.presence.activities[0].state);
+      await redisClient.setEx(
+        username,
+        DEFAULT_EXPIRATION,
+        newPresence.member.presence.activities[0].state
+      );
       console.log(`User ${username} started streaming!`);
-      const {state: gameName, url: activityUrl, details} = newPresence.member.presence.activities[0];
+      const {
+        state: gameName,
+        url: activityUrl,
+        details,
+      } = newPresence.member.presence.activities[0];
       getGameId(gameName).then((Id) => {
         if (Id.length > 0) {
           getGameArt(Id[0].id).then((coverData) => {
             let url = `https://images.igdb.com/igdb/image/upload/t_720p/${coverData[0].image_id}.jpg`;
             const streamEmbed = new MessageEmbed()
               .setTitle(details)
-              .addFields(
-                { name: 'Peli', value: `${gameName}` },
-              )
-              .setAuthor({ name: `${newPresence.user.username}`, iconURL: `${newPresence.user.avatarURL()}` })
+              .addFields({ name: 'Peli', value: `${gameName}` })
+              .setAuthor({
+                name: `${newPresence.user.username}`,
+                iconURL: `${newPresence.user.avatarURL()}`,
+              })
               .setImage(url)
               .setTimestamp();
             client.channels.cache.get(process.env.STRIIMI_KANAVA_ID).send({
               content: `Hei kaikki! ${newPresence.user.tag} aloitti striimin osoitteessa ${activityUrl}.`,
-              embeds: [streamEmbed]
+              embeds: [streamEmbed],
             });
           });
         } else {
           const streamEmbed = new MessageEmbed()
             .setTitle(details)
-            .addFields(
-              { name: 'Peli', value: `${gameName}` },
-            )
-            .setAuthor({ name: `${newPresence.user.username}`, iconURL: `${newPresence.user.avatarURL()}` })
+            .addFields({ name: 'Peli', value: `${gameName}` })
+            .setAuthor({
+              name: `${newPresence.user.username}`,
+              iconURL: `${newPresence.user.avatarURL()}`,
+            })
             .setImage(newPresence.user.avatarURL())
             .setTimestamp();
           client.channels.cache.get(process.env.STRIIMI_KANAVA_ID).send({
             content: `Hei kaikki! ${newPresence.user.tag} aloitti striimin osoitteessa ${activityUrl}.`,
-            embeds: [streamEmbed]
+            embeds: [streamEmbed],
           });
         }
       });
@@ -79,10 +107,9 @@ client.on('presenceUpdate', async (oldPresence, newPresence) => {
       return;
     }
   }
-
 });
 
-client.on('messageCreate', msg => {
+client.on('messageCreate', (msg) => {
   if (msg.author.bot) return;
 
   const [command, ...args] = msg.content.split(' ');
@@ -90,14 +117,16 @@ client.on('messageCreate', msg => {
   //Tarkista onko kanava striimi-ilmoitukset ja onko viestissä URL muualta kuin Twitchistä.
   if (msg.channel.id === process.env.STRIIMI_KANAVA_ID) {
     if (containsUrl(msg.content) && !containsTwitchUrl(msg.content)) {
-      msg.channel.send(`${msg.author} Anteeksi, mutta tämä tekstikanava sallii vain Twitch-linkit`);
+      msg.channel.send(
+        `${msg.author} Anteeksi, mutta tämä tekstikanava sallii vain Twitch-linkit`
+      );
       msg.delete();
     }
   }
 
   //Komennot alkavat tästä
   if (command.startsWith(PREFIX)) {
-    switch(command.toLowerCase()) {
+    switch (command.toLowerCase()) {
       case '!twitch':
         msg.channel.send(`https://twitch.tv/${args[0]}`);
         break;
@@ -105,15 +134,24 @@ client.on('messageCreate', msg => {
         const infoEmbed = new MessageEmbed()
           .setTitle('Botin komennot')
           .addFields(
-            { name: '!twitch', value: 'Lähettää kanavalle Twitch-linkin striimiisi. Esim. !twitch tartaroksentatit' },
-            { name: '!komennot', value: 'Jos nyt on pakko tämä kertoa, niin luet tätä jo!' },
+            {
+              name: '!twitch',
+              value:
+                'Lähettää kanavalle Twitch-linkin striimiisi. Esim. !twitch tartaroksentatit',
+            },
+            {
+              name: '!komennot',
+              value: 'Jos nyt on pakko tämä kertoa, niin luet tätä jo!',
+            }
           )
           .setTimestamp()
           .setFooter({ text: 'Tartarosbotti' });
         msg.channel.send({ embeds: [infoEmbed] });
         break;
       default:
-        msg.reply(`Antamaasi komentoa ${command} ei löydy. Tarkista komennot kirjoittamalla !komennot`);
+        msg.reply(
+          `Antamaasi komentoa ${command} ei löydy. Tarkista komennot kirjoittamalla !komennot`
+        );
     }
   }
 });
